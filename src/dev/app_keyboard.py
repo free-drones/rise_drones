ad5e4845fe32bce99b07584a9261ed10c2701f61
Andreas Gising <a.gising@gmail.com>
2022-10-27 09:10:14 +0200

    fix app_keyboard

--- src/dev/app_keyboard.py
+++ src/dev/app_keyboard.py
@@ -549,7 +549,7 @@ class KeyboardClient(dss.client.Client):
             else:
               self._drone_name = answer['id']
               time.sleep(1.0)
-              self.connect(answer['ip'], answer['port'])
+              self.connect(answer['ip'], answer['port'], answer['id'])
               print(f"Successfully connected to drone: [{self._drone_name}]")
         elif key == '6':
           if self._dss is not None:
@@ -562,7 +562,7 @@ class KeyboardClient(dss.client.Client):
             else:
               self._drone_name = answer['id']
               time.sleep(1.0)
-              self.connect(answer['ip'], answer['port'])
+              self.connect(answer['ip'], answer['port'], answer['id'])
               print(f"Successfully connected to drone: [{self._drone_name}]")
         elif key == '7':
           if self._dss is not None:
